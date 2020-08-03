ba9f8a55ddfe0fa8e246518c2ec005c8f1962b43
ruios <ruiosli@kreditme.id>
2020-08-03 14:43:40 +0800

    update

--- leetcode/两数之和.js
+++ leetcode/两数之和.js
@@ -32,6 +32,7 @@ var twoSum = function(nums, target) {
     }
 };
 
+// 拿空间换时间 （自身需要遍历两次的时候， 用一个对象来记录一个遍历，使得只需要遍历一次）
 var twoSum2 = function(nums, target) {
     var pre = {};
     for(var i = 0; i < nums.length; i ++){
@@ -43,6 +44,27 @@ var twoSum2 = function(nums, target) {
     }
 };
 
+// 双指针法  这个有问题： 因为需要返回数组下标， 但是 双指针法需要先对数组排序，这样无法返回之前的下标
+var twoSum3 = function (nums, target) {
+    var hash = {};
+    for(var i = 0; i < nums.length; i++){
+        hash[nums[i]] = i;
+    }
+    nums.sort((a, b) => a-b);
+    var first = 0;
+    var last = nums.length-1;
+    while(last > first) {
+        var sum = nums[first] + nums[last];
+        if(sum > target) {
+            last--;
+        }else if (sum < target) {
+            first ++;
+        }else{
+            return [hash[nums[first]], hash[nums[last]]]
+        }
+    }
+}
+
 //test
 
 
@@ -50,12 +72,32 @@ test(twoSum);
 
 test(twoSum2)
 
+test(twoSum3)
+
 
 function test(func){
     describe(`${func.name}`, () => {
         it('test1', () => {
             const input = [2, 7, 11, 15];
             const input2 = 9;
+            const output = [0 ,1];
+            expect(func(input, input2)).toEqual(output);
+        });
+    })
+
+    describe(`${func.name}`, () => {
+        it('test2', () => {
+            const input = [3,2,4];
+            const input2 = 6;
+            const output = [1 ,2];
+            expect(func(input, input2)).toEqual(output);
+        });
+    })
+
+    describe(`${func.name}`, () => {
+        it('test2', () => {
+            const input = [3,3];
+            const input2 = 6;
             const output = [0 ,1];
             expect(func(input, input2)).toEqual(output);
         });
