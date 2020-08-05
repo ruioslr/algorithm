311551f6e6f79567abd9103f110a80860951637c
ruios <ruioslr@163.com>
2020-08-05 18:28:38 +0800

    add: KMP算法

---
+++ 编程范式/KMP算法/暴力匹配.js
@@ -0,0 +1,48 @@
+/*
+   从 字符串 S 中 匹配 P 若 匹配成功则返回 index， 否则返回 -1
+*/
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var search = function (s, p) {
+  var i = 0,
+    j = 0;
+
+  while (i < s.length && j < p.length) {
+    if (s[i] === p[j]) {
+      i++;
+      j++;
+    } else {
+      i = i - j + 1;
+      j = 0;
+    }
+  }
+  if (j === p.length) {
+    return i - j;
+  } else {
+    return -1;
+  }
+};
+
+test(search);
+
+function test(func) {
+  describe(`${func.name}`, () => {
+    it("test1", () => {
+      const input = "abcde";
+      const input2 = "bcd";
+      const output = 1;
+      expect(func(input, input2)).toEqual(output);
+    });
+
+    it("test2", () => {
+      const input = "abcbe";
+      const input2 = "xxx";
+      const output = -1;
+      expect(func(input, input2)).toEqual(output);
+    });
+  });
+}
---
+++ 编程范式/KMP算法/算法实现.js
@@ -0,0 +1,59 @@
+/*
+   从 字符串 S 中 匹配 P 若 匹配成功则返回 index， 否则返回 -1
+*/
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var search = function (s, p) {
+  var i = 0,
+    j = -1;
+  var next = getNext(p);
+  function getNext(p) {
+    var i = 0;
+    var next = [];
+    next[0] = -1;
+    var k = -1;
+    while(i < p.length-1){
+      if(k == -1 || p[i] === p[k]){
+        next[++i] = ++ k;
+      }else{
+        k = next[k];
+      }
+    }
+    return next;
+  }
+
+  while (i < s.length && j < p.length) {
+    if (s[i] === p[j] || j === -1) {
+      i++;
+      j++;
+    } else {
+      j = next[j];
+    }
+  }
+
+  return j === p.length ? i - j : -1;
+};
+
+test(search);
+
+function test(func) {
+  describe(`${func.name}`, () => {
+    it("test1", () => {
+      const input = "abcde";
+      const input2 = "bcd";
+      const output = 1;
+      expect(func(input, input2)).toEqual(output);
+    });
+
+    it("test2", () => {
+      const input = "abcbe";
+      const input2 = "xxx";
+      const output = -1;
+      expect(func(input, input2)).toEqual(output);
+    });
+  });
+}
