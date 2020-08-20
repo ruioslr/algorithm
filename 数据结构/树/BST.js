931a4602f299be12eb8635937dd46d0b3c971ee7
ruios <ruioslr@163.com>
2020-08-20 13:54:18 +0800

    update

---
+++ 数据结构/树/BST.js
@@ -0,0 +1,32 @@
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  // 插入
+  insert = (data) => {
+
+  }
+  // 查找
+  search = (data) => {}
+  // 中序遍历
+  inOrderTraverse = () => {}
+  // 先序遍历
+  preOrderTraverse = () => {}
+  // 后序遍历
+  postOrderTraverse = () => {}
+  删除
+  remove = (data) => {}
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+
+
+
+
