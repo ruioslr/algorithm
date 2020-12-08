7b5cf30ffb5d2a7534c7b1bab6f4c15057b5505a
ruios <ruioslr@163.com>
2020-12-08 15:31:03 +0800

    update

--- 算法范式和方法/回溯法/八皇后问题.js
+++ 算法范式和方法/回溯法/八皇后问题.js
@@ -2,24 +2,44 @@
   在8×8格的国际象棋上摆放八个皇后，使其不能互相攻击，即任意两个皇后都不能处于同一行、同一列或同一斜线上，问一共有多少种摆法。
 
 */
+// n皇后
+function solution(n) {
+  const res = [];
+  const board = new Array(n).fill('.').map(_ => new Array(n).fill('.'));
 
-function solution() {
-  const board = [];
-  let total = 0;
-
-}
-
-
-function push(board, k, total) {
-  if(k >= board.length){
-    return ++total;
+  function backTrace(k){
+    if(k === n){
+      res.push(board.slice())
+      return;
+    }
+    for(let i = 0; i < n; i++){
+      if(isOk(k, i)){
+        board[k][i] = 'Q';
+        backTrace(k+1);
+        board[k][i] = '.';
+      }
+    }
   }
 
-  for(let i = 0; i < board.length; i++){
-    board[k][i] = 0;
-    if(check(k, i)){
-      board[k][i] = 1;
-      push(k+1);
+  function isOk(row, col){
+    for(let i = 0; i < col; i++){
+      if(board[row][i] === 'Q'){
+        return false;
+      }
     }
+
+    for(let m = row-1, j = col -1; m >= 0, j >= 0; m--, j--){
+      if(board[m][j] === 'Q'){
+        return false;
+      }
+    }
+
+    for(let m = row-1, j = col + 1; m >=0, j < n; m--, j++){
+      if(board[m][j] === 'Q'){
+        return false;
+      }
+    }
+
+    return true;
   }
 }
